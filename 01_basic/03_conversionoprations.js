fa458aa2112280d811339426a710a9eef69e84fe
Deepak Garg <121867209+deepak01042003@users.noreply.github.com>
2024-03-18 09:02:09 +0000

    numbers and maths

---
+++ 01_basic/03_conversionoprations.js
@@ -0,0 +1,14 @@
+let score= false
+let valueinnumber=String(score)
+// console.log(typeof NULL)
+// console.log(typeof valueinnumber)
+// console.log(valueinnumber)
+//console.log(score)
+//******oprations********
+let value= 5
+let ok=2
+let negvalue=-value
+console.log(value+"u")
+console.log("depps"+value+ok)
+// console.log(negvalue)
+console.log("2"=== 2)
---
+++ 01_basic/04_numsansmaths.js
@@ -0,0 +1,18 @@
+// let score= 126980900
+// console.log(score)
+// console.log(typeof score)
+// console.log(score.toString())
+// console.log(score.toFixed(2))
+// console.log(score)
+// console.log(score.toPrecision(2))
+// console.log(score.toLocaleString('en-IN'))
+// ++++++++++++++++MATHS+++++++++++++++++++++++++++++++++++++++++++++++
+
+// 
+// console.log(Math.round(99.5299))
+// console.log(Math.ceil(89.89))
+// console.log(Math.floor(89.99))
+let min=1
+let max=6
+
+console.log((Math.floor(Math.random()*(max-min+1)))+min)
